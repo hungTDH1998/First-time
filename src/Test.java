3fb0399f398345622485ac0f0355b8d004456279
hungTDH1998 <68639668+hungTDH1998@users.noreply.github.com>
2020-04-30 14:44:21 +0700

    add everything

---
+++ src/Test.java
@@ -0,0 +1,8 @@
+
+public class Test {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello Java");
+        String st = "This is my very first time i used Github";
+        System.out.println(st);
+}
+}
